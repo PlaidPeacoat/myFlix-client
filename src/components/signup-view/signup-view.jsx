92339cc8d8defe5287d950d65e628de024275c1c
PlaidPeacoat <114315812+PlaidPeacoat@users.noreply.github.com>
2023-03-15 21:06:48 -0400

    implementing react bootstrap

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 export const SignupView = () => {
 
     const handleSubmit = (event) => {};
   
     return (
-      <form onSubmit={handleSubmit}>
-        <button type="submit">Submit</button>
-      </form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3" 
+          />
+        </Form.Group>
+  
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     );
   };
+  
   
